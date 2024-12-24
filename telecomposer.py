c043bcbb30cb071ac96b8cc984fa28f7a9df81cd
Blaze <luissc.outlier@gmail.com>
2024-12-24 14:53:49 +0000

    delete unnecessaries

--- telecomposer.py
+++
@@ -1,7 +0,0 @@
-#app > div.main-content > div.columns.is-multiline > div:nth-child(3) > div > div > div > div > div > div > div > div > div:nth-child(1) > div.day-overview-column > div:nth-child(1)
-
-cur_element > a .click
-
-sign up or in
-
-
